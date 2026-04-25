98642cc412f553acb4157e3e0f19c1c91a4e86d7
Dmitry64 <nosdm64@gmail.com>
2026-04-25 18:26:21 +0300

    Добавляет секцию girls-list релиз1

--- src/widgets/girls-list/girls-list.jsx
+++ src/widgets/girls-list/girls-list.jsx
@@ -1,48 +1,96 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/swiper-bundle.css';
-import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css/free-mode';
+import 'swiper/css/effect-creative';
+import { FreeMode, Pagination, EffectCreative } from 'swiper/modules';
 import { girlsData } from '../../shared/data/girls-data';
 import './girls-list.css';
+import { useWindowWidth } from '../../shared/hooks/useWindowWidth';
 
 const GirlsList = () => {
+  const windowWidth = useWindowWidth();
+  const isMobile = windowWidth < 480;
+  const isTablet = windowWidth >= 480;
   return (
     <section className="girls-list">
+      <h2 className="girls-list--title">Это наши девушки</h2>
       <div className="girls-list--wrapper container">
-        <Swiper
-          modules={[Navigation, Pagination]}
-          navigation={true}
-          pagination={true}
-          loop={true}
-          breakpoints={{
-            320: {
-              slidesPerView: 1,
-              navigation: false,
-              centeredSlides: true,
-            },
-            550: {
-              slidesPerView: 2,
-              navigation: false,
-              centeredSlides: true,
-              spaceBetween: 30,
-            },
-          }}
-        >
-          {girlsData.map((item) => (
-            <SwiperSlide key={item.id}>
-              <div className="girls-list--slide">
-                <picture>
-                  <source type="image/webp" srcSet={item.pathWebp} />
-                  <img
-                    src={item.path}
-                    width={250}
-                    height={333}
-                    alt={item.alt}
-                  />
-                </picture>
-              </div>
-            </SwiperSlide>
-          ))}
-        </Swiper>
+        {isMobile && (
+          <Swiper
+            slidesPerView={1}
+            modules={[Pagination, EffectCreative]}
+            pagination={{ clickable: true }}
+            loop={true}
+            speed={0.3}
+            effect="creative"
+            creativeEffect={{
+              prev: {
+                // shadow: true,
+                translate: [0, 0, -500],
+              },
+              next: {
+                translate: ['100%', 0, 0],
+              },
+            }}
+          >
+            {girlsData.map((item) => (
+              <SwiperSlide key={item.id}>
+                <div className="girls-list--slide">
+                  <picture>
+                    <source type="image/webp" srcSet={item.pathWebp} />
+                    <img
+                      src={item.path}
+                      width={item.width}
+                      height={item.height}
+                      alt={item.alt}
+                    />
+                  </picture>
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        )}
+        {isTablet && (
+          <Swiper
+            // slidesPerView={3}
+            // spaceBetween={25}
+            modules={[Pagination, FreeMode]}
+            pagination={{ clickable: true }}
+            loop={true}
+            freeMode={true}
+            speed={0.3}
+            breakpoints={{
+              320: {
+                slidesPerView: 1,
+                spaceBetween: 20,
+              },
+              480: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+              },
+              720: {
+                slidesPerView: 3,
+                spaceBetween: 25,
+              },
+            }}
+          >
+            {girlsData.map((item) => (
+              <SwiperSlide key={item.id}>
+                <div className="girls-list--slide">
+                  <picture>
+                    <source type="image/webp" srcSet={item.pathWebp} />
+                    <img
+                      src={item.path}
+                      width={item.width}
+                      height={item.height}
+                      alt={item.alt}
+                    />
+                  </picture>
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        )}
       </div>
     </section>
   );
