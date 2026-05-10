8be8bb2d16644dffbf55e209a84f71e5c98356e1
Dmitry64 <nosdm64@gmail.com>
2026-05-10 20:56:43 +0300

    Изменяет размер фото

--- src/shared/data/girls-data.js
+++ src/shared/data/girls-data.js
@@ -4,120 +4,120 @@ export const girlsData = [
     path: '/img/girls/girl01.jpg',
     pathWebp: '/img/girls/girl01.webp',
     alt: 'Фотография девушек',
-    width: '300',
-    height: '400'
+    width: '260',
+    height: '347'
   },
     {
     id: 'c2e92736b5e841c0b1c746814dedb324',
     path: '/img/girls/girl02.jpg',
     pathWebp: '/img/girls/girl02.webp',
     alt: 'Фотография девушек',
-    width: '300',
-    height: '400'
+    width: '260',
+    height: '347'
   },
   {
     id: '8af633251f7b47ffa271e700da486c42',
     path: '/img/girls/girl03.jpg',
     pathWebp: 'img/girls/girl03.webp',
     alt: 'Фотография девушек',
-    width: '300',
-    height: '400'
+    width: '260',
+    height: '347'
   },
   {
     id: 'ba8cbb4074164a6ba6abee0eeff742d3',
     path: '/img/girls/girl04.jpg',
     pathWebp: '/img/girls/girl04.webp',
     alt: 'Фотография девушек',
-    width: '300',
-    height: '400'
+    width: '260',
+    height: '347'
   },
   {
     id: 'c64646ce6d574a5db32e31f961504558',
     path: '/img/girls/girl05.jpg',
     pathWebp: '/img/girls/girl05.webp',
     alt: 'Фотография девушек',
-    width: '300',
-    height: '400'
+    width: '260',
+    height: '347'
   },
     {
     id: 'c64646ce6d574a5db32e31f9615fhegd8',
     path: '/img/girls/girl06.jpg',
     pathWebp: '/img/girls/girl06.webp',
     alt: 'Фотография девушек',
-    width: '300',
-    height: '400'
+    width: '260',
+    height: '347'
   },
     {
     id: 'c2e92736b5e841c0b1c746814dgfqc86',
     path: '/img/girls/girl07.jpg',
     pathWebp: '/img/girls/girl07.webp',
     alt: 'Фотография девушек',
-    width: '300',
-    height: '400'
+    width: '260',
+    height: '347'
   },
   {
     id: '8af633251f7b47ffa271e700da4gskue42',
     path: '/img/girls/girl08.jpg',
     pathWebp: '/img/girls/girl08.webp',
     alt: 'Фотография девушек',
-    width: '300',
-    height: '400'
+    width: '260',
+    height: '347'
   },
   {
     id: 'ba8cbb4074164a6ba6abee0eeff74289',
     path: '/img/girls/girl09.jpg',
     pathWebp: '/img/girls/girl09.webp',
     alt: 'Фотография девушек',
-    width: '300',
-    height: '400'
+    width: '260',
+    height: '347'
   },
   {
     id: 'c64646ce6d574a5db32e31f961504125',
     path: '/img/girls/girl10.jpg',
     pathWebp: '/img/girls/girl10.webp',
     alt: 'Фотография девушек',
-    width: '300',
-    height: '400'
+    width: '260',
+    height: '347'
   },
     {
     id: 'c64646ce6d574a5db32e31f961504asd8',
     path: '/img/girls/girl11.jpg',
     pathWebp: '/img/girls/girl11.webp',
     alt: 'Фотография девушек',
-    width: '300',
-    height: '400'
+    width: '260',
+    height: '347'
   },
     {
     id: 'c64646ce6d574a5db32e31f961504u648',
     path: '/img/girls/girl12.jpg',
     pathWebp: '/img/girls/girl12.webp',
     alt: 'Фотография девушек',
-    width: '300',
-    height: '400'
+    width: '260',
+    height: '347'
   },
   {
     id: 'c64646ce6d574a5db32e31f961504s712',
     path: '/img/girls/girl13.jpg',
     pathWebp: '/img/girls/girl13.webp',
     alt: 'Фотография девушек',
-    width: '300',
-    height: '400'
+    width: '260',
+    height: '347'
   },
   {
     id: 'c64646ce6d574a5db35631f961504189',
     path: '/img/girls/girl14.jpg',
     pathWebp: '/img/girls/girl14.webp',
     alt: 'Фотография девушек',
-    width: '300',
-    height: '400'
+    width: '260',
+    height: '347'
   },
     {
     id: 'c64646ce6d574a5dbqwer31f961504125',
     path: '/img/girls/girl15.jpg',
     pathWebp: '/img/girls/girl15.webp',
     alt: 'Фотография девушек',
-    width: '300',
-    height: '400'
+    width: '260',
+    height: '347'
   },
 
   //   {
@@ -125,7 +125,7 @@ export const girlsData = [
   //   path: '/img/girls/girls16.jpg',
   //   pathWebp: '/img/girls/girls16.webp',
   //   alt: 'Фотография девушек',
-  //   width: '300',
+  //   width: '260',
   //   height: '304'
   // },
 
