feb053982c5a01933cbcb85f1cf00941b49f31d9
Dmitry64 <nosdm64@gmail.com>
2026-05-11 07:07:47 +0300

    Добавляет фотографии man

--- src/shared/data/men-data.js
+++ src/shared/data/men-data.js
@@ -2,7 +2,6 @@ export const menData = [
   {
     id: 'c64646ce6d574a5db32e31f9615033b3',
     path: '/img/men/man01.jpg',
-    pathWebp: '/img/men/man01.webp',
     alt: 'Фотография мужчин',
     width: '260',
     height: '347'
@@ -10,7 +9,6 @@ export const menData = [
     {
     id: 'c2e92736b5e841c0b1c746814dedb324',
     path: '/img/men/man02.jpg',
-    pathWebp: '/img/men/man02.webp',
     alt: 'Фотография мужчин',
     width: '260',
     height: '347'
@@ -18,7 +16,6 @@ export const menData = [
   {
     id: '8af633251f7b47ffa271e700da486c42',
     path: '/img/men/man03.jpg',
-    pathWebp: '/img/men/man03.webp',
     alt: 'Фотография мужчин',
     width: '260',
     height: '347'
@@ -26,7 +23,6 @@ export const menData = [
   {
     id: 'ba8cbb4074164a6ba6abee0eeff742d3',
     path: '/img/men/man04.jpg',
-    pathWebp: '/img/men/man04.webp',
     alt: 'Фотография мужчин',
     width: '260',
     height: '347'
@@ -34,7 +30,6 @@ export const menData = [
   {
     id: 'c64646ce6d574a5db32e31f961504558',
     path: '/img/men/man05.jpg',
-    pathWebp: '/img/men/man05.webp',
     alt: 'Фотография мужчин',
     width: '260',
     height: '347'
@@ -42,7 +37,6 @@ export const menData = [
     {
     id: 'c64646ce6d574a5db32e31f9615fhegd8',
     path: '/img/men/man06.jpg',
-    pathWebp: '/img/men/man06.webp',
     alt: 'Фотография мужчин',
     width: '260',
     height: '347'
@@ -50,7 +44,6 @@ export const menData = [
     {
     id: 'c2e92736b5e841c0b1c746814dgfqc86',
     path: '/img/men/man07.jpg',
-    pathWebp: '/img/men/man07.webp',
     alt: 'Фотография мужчин',
     width: '260',
     height: '347'
@@ -58,7 +51,6 @@ export const menData = [
   {
     id: '8af633251f7b47ffa271e700da4gskue42',
     path: '/img/men/man08.jpg',
-    pathWebp: '/img/men/man08.webp',
     alt: 'Фотография мужчин',
     width: '260',
     height: '347'
@@ -66,11 +58,78 @@ export const menData = [
   {
     id: 'ba8cbb4074164a6ba6abee0eeff74289',
     path: '/img/men/man09.jpg',
-    pathWebp: '/img/men/man09.webp',
     alt: 'Фотография мужчин',
     width: '260',
     height: '347'
   },
-
-
+    {
+    id: 'man10',
+    path: '/img/men/man10.jpg',
+    alt: 'Фотография мужчин',
+    width: '260',
+    height: '347'
+  },
+    {
+    id: 'man11',
+    path: '/img/men/man11.jpg',
+    alt: 'Фотография мужчин',
+    width: '260',
+    height: '347'
+  },
+  {
+    id: 'man12',
+    path: '/img/men/man12.jpg',
+    alt: 'Фотография мужчин',
+    width: '260',
+    height: '347'
+  },
+  {
+    id: 'man13',
+    path: '/img/men/man13.jpg',
+    alt: 'Фотография мужчин',
+    width: '260',
+    height: '347'
+  },
+  {
+    id: 'man14',
+    path: '/img/men/man14.jpg',
+    alt: 'Фотография мужчин',
+    width: '260',
+    height: '347'
+  },
+    {
+    id: 'man15',
+    path: '/img/men/man15.jpg',
+    alt: 'Фотография мужчин',
+    width: '260',
+    height: '347'
+  },
+    {
+    id: 'man16',
+    path: '/img/men/man16.jpg',
+    alt: 'Фотография мужчин',
+    width: '260',
+    height: '347'
+  },
+  {
+    id: 'man17',
+    path: '/img/men/man17.jpg',
+    alt: 'Фотография мужчин',
+    width: '260',
+    height: '347'
+  },
+  {
+    id: 'man18',
+    path: '/img/men/man18.jpg',
+    alt: 'Фотография мужчин',
+    width: '260',
+    height: '347'
+  },
+    {
+    id: 'man19',
+    path: '/img/men/man19.jpg',
+    alt: 'Фотография мужчин',
+    width: '260',
+    height: '347'
+  },
 ]
